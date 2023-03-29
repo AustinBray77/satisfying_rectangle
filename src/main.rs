988ff8b28647e40bae2681697e20fbea41079a68
deoignition <93416177+AustinBray77@users.noreply.github.com>
2023-03-29 19:22:39 -0400

    Updates and Bug Fixes: -Added spawning after sqaure hits corner -Added random start speed and angle -Added synchronized color changing -Fixed various minor bugs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,66 @@
 use bevy::prelude::*;
+use rand::Rng;
 
-#[derive(Component)]
+#[derive(Resource, Default)]
 struct RectFactors {
-    entity: Entity,
-    angle: f32,
-    speed: f32,
     max_x: f32,
     min_x: f32,
     max_y: f32,
     min_y: f32,
 }
 
+#[derive(Resource, Default)]
+struct SpawnFlags {
+    spawn_rects: i32,
+    color: Color,
+}
+
+#[derive(Component)]
+struct MoveAbleRect {
+    angle: f32,
+    speed: f32,
+    just_spawned: bool,
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .insert_resource(RectFactors { ..default() })
+        .insert_resource(SpawnFlags {
+            color: Color::rgb(1., 1., 0.),
+            ..default()
+        })
         .add_startup_system(setup)
         .add_system(move_rectangle)
+        .add_system(spawn_rectangles)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn spawn_rectangles(mut commands: Commands, mut spawn_flags: ResMut<SpawnFlags>) {
+    let mut rng = rand::thread_rng();
+
+    for _i in 1..=spawn_flags.spawn_rects {
+        commands
+            .spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: spawn_flags.color,
+                    custom_size: Some(Vec2::new(50., 50.)),
+                    ..default()
+                },
+                transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+                ..default()
+            })
+            .insert(MoveAbleRect {
+                angle: rng.gen_range(0..=360) as f32,
+                speed: rng.gen_range(1..=5) as f32,
+                just_spawned: false,
+            });
+    }
+
+    spawn_flags.spawn_rects = 0;
+}
+
+fn setup(mut commands: Commands, mut rect_factors: ResMut<RectFactors>) {
     commands.spawn(Camera2dBundle::default());
 
     let bounds_x = 400.;
@@ -38,7 +79,8 @@ fn setup(mut commands: Commands) {
         ..default()
     });
 
-    let id = commands
+    let mut rng = rand::thread_rng();
+    commands
         .spawn(SpriteBundle {
             sprite: Sprite {
                 color: Color::rgb(1., 1., 0.),
@@ -48,68 +90,68 @@ fn setup(mut commands: Commands) {
             transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
             ..default()
         })
-        .id();
+        .insert(MoveAbleRect {
+            angle: rng.gen_range(0..=360) as f32,
+            speed: rng.gen_range(1..=5) as f32,
+            just_spawned: false,
+        });
 
     let corner1 = Vec2::new(-bounds_x, bounds_x);
     let corner2 = Vec2::new(bounds_x, -bounds_y);
-
-    commands.spawn(RectFactors {
-        entity: id,
-        angle: 0.,
-        speed: 5.,
-        max_x: corner2.x,
-        min_x: corner1.x,
-        max_y: corner1.y,
-        min_y: corner2.y,
-    });
+    rect_factors.max_x = corner2.x;
+    rect_factors.min_x = corner1.x;
+    rect_factors.max_y = corner1.y;
+    rect_factors.min_y = corner2.y;
 }
 
 fn move_rectangle(
-    mut rect_query: Query<(Entity, &mut Transform, &mut Sprite)>,
-    mut factors_query: Query<(Entity, &mut RectFactors)>,
+    mut rect_query: Query<(Entity, &mut Transform, &mut Sprite, &mut MoveAbleRect)>,
+    rect_factors: Res<RectFactors>,
+    mut spawn_flags: ResMut<SpawnFlags>,
 ) {
-    let mut factors = factors_query.single_mut().1;
+    if spawn_flags.color.r() == 1. && 0. < spawn_flags.color.g() {
+        spawn_flags.color += Color::rgb(0., -0.01, 0.01)
+    } else if spawn_flags.color.b() == 1. && 0. < spawn_flags.color.r() {
+        spawn_flags.color += Color::rgb(-0.01, 0.01, 0.)
+    } else if spawn_flags.color.g() == 1. && 0. < spawn_flags.color.b() {
+        spawn_flags.color += Color::rgb(0.01, 0., -0.01)
+    }
 
-    let bound_factor = 25. + factors.speed;
+    if spawn_flags.color.r() > 1. {
+        spawn_flags.color = Color::rgb(1., spawn_flags.color.g(), spawn_flags.color.b());
+    }
 
-    for (entity, mut transform, mut sprite) in rect_query.iter_mut() {
-        if entity == factors.entity {
-            if transform.translation.x + bound_factor >= factors.max_x
-                || transform.translation.x - bound_factor <= factors.min_x
-                || transform.translation.y + bound_factor >= factors.max_y
-                || transform.translation.y - bound_factor <= factors.min_y
-            {
-                factors.angle = 180. + factors.angle;
+    if spawn_flags.color.g() > 1. {
+        spawn_flags.color = Color::rgb(spawn_flags.color.r(), 1., spawn_flags.color.b());
+    }
+    if spawn_flags.color.b() > 1. {
+        spawn_flags.color = Color::rgb(spawn_flags.color.r(), spawn_flags.color.g(), 1.);
+    }
+
+    for (_entity, mut transform, mut sprite, mut moveable_rect) in rect_query.iter_mut() {
+        let bound_factor = 25. + moveable_rect.speed;
+
+        if transform.translation.x + bound_factor >= rect_factors.max_x
+            || transform.translation.x - bound_factor <= rect_factors.min_x
+            || transform.translation.y + bound_factor >= rect_factors.max_y
+            || transform.translation.y - bound_factor <= rect_factors.min_y
+        {
+            moveable_rect.angle = 180. + moveable_rect.angle;
+
+            if !moveable_rect.just_spawned {
+                spawn_flags.spawn_rects += 1;
+                moveable_rect.just_spawned = true;
             }
-
-            transform.translation += Vec3::new(
-                factors.speed * factors.angle.cos(),
-                factors.speed * factors.angle.sin(),
-                0.,
-            );
-
-            factors.angle += factors.speed / 100000.;
-
-            if sprite.color.r() == 1. && 0. < sprite.color.g() {
-                sprite.color += Color::rgb(0., -0.01, 0.01)
-            } else if sprite.color.b() == 1. && 0. < sprite.color.r() {
-                sprite.color += Color::rgb(-0.01, 0.01, 0.)
-            } else if sprite.color.g() == 1. && 0. < sprite.color.b() {
-                sprite.color += Color::rgb(0.01, 0., -0.01)
-            }
-
-            if sprite.color.r() > 1. {
-                sprite.color = Color::rgb(1., sprite.color.g(), sprite.color.b());
-            }
-
-            if sprite.color.g() > 1. {
-                sprite.color = Color::rgb(sprite.color.r(), 1., sprite.color.b());
-            }
-            if sprite.color.b() > 1. {
-                sprite.color = Color::rgb(sprite.color.r(), sprite.color.g(), 1.);
-            }
-
-            break;
+        } else {
+            moveable_rect.just_spawned = false;
         }
+
+        transform.translation += Vec3::new(
+            moveable_rect.speed * moveable_rect.angle.cos(),
+            moveable_rect.speed * moveable_rect.angle.sin(),
+            0.,
+        );
+
+        sprite.color = spawn_flags.color;
     }
 }
